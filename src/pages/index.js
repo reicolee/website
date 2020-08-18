4f86e0b335a823cd4cfe590d3eea40944ed1ebad
Reico Lee <reico@withdouble.com>
2020-08-17 21:41:50 -0400

    🎉 add node sass, init project

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
@@ -16,7 +16,7 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  </>
 )
 
 export default IndexPage
