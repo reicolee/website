81049eb0fc06024d1533da0ca9f4252943337df4
Reico Lee <reico@withdouble.com>
2020-08-18 03:56:57 -0400

    ✨ configure favicon

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const TAGLINE = 'I build things on the web, and I love what I do.'
+
+export const DESCRIPTION = "I'm a full stack software engineer based in NYC, specializing in building \
+web applications using frameworks such as NodeJS, React, Redux, GraphQL, Django, etc."
+
+export const ABOUT_ME = "Ever since I started programming 4 years ago, \
+I have been thrilled by what I can create and build with code. My goal has always been sharing this passion \
+with users"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+
+import { TAGLINE, DESCRIPTION, ABOUT_ME } from './constants'
 
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
