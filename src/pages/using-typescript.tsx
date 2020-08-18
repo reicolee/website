b8210347f44861721271bbb205f8bc419e83a614
Reico Lee <reico@withdouble.com>
2020-08-18 05:24:51 -0400

    🐛 fix imports to use absolute path

--- src/pages/using-typescript.tsx
+++ src/pages/using-typescript.tsx
@@ -2,8 +2,8 @@
 import React from "react"
 import { PageProps, Link, graphql } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/SEO"
+import Layout from "src/components/Layout"
+import SEO from "src/components/SEO"
 
 type DataProps = {
   site: {
