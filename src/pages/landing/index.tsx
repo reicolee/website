8ec5de76eb8aa157bec66eeacdda3b4914b23118
Reico Lee <reico@withdouble.com>
2020-08-21 19:22:21 -0400

    💄 update intro section

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -33,7 +33,6 @@ const IndexPage = () => {
             <p className="greeting">Hi,</p>
             <h1 className="heading">I'm <span>Reico</span> Lee.</h1>
             <h2 className="tagline">{tagline}</h2>
-            <p className="description">{description}</p>
             <p className="companies">Currently <a className="companyLink" target="_blank" rel="nofollow noopener noreferrer" href="https://withdouble.com">@Double</a>.
             Formerly <a className="companyLink" target="_blank" rel="nofollow noopener noreferrer" href="https://getbento.com/">@BentoBox</a>.</p>
             <a className="resumeBtn" aria-label="Reico's Resume" target="_blank" rel="nofollow noopener noreferrer" href={'../../reico-resume.pdf'}>
@@ -50,7 +49,7 @@ const IndexPage = () => {
         <div className="content">
           <h2 className="header">About me.</h2>
           <div className="text">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>{description}</p>
           </div>
         </div>
       </section>
