7257b4c8a92d8f4d7b8e61f8d447d59838b5d62a
Reico Lee <reico@withdouble.com>
2020-08-21 20:12:24 -0400

    💄 about section styling

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -48,8 +48,16 @@ const IndexPage = () => {
       <section className="about">
         <div className="content">
           <h2 className="header">About me.</h2>
-          <div className="text">
+          <div className="box description">
             <p>{description}</p>
+          </div>
+          <div className="box background">
+            <h3 className="box-header">Background.</h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+          <div className="box skills">
+          <h3 className="box-header">Skills.</h3>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </div>
         </div>
       </section>
