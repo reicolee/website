2d7c3381ccf7d4ea1f5a185f2b08bedc8acb19ab
Reico Lee <reico@withdouble.com>
2020-08-23 02:15:01 -0400

    💩 commnet out work section

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -78,15 +78,15 @@ const IndexPage = () => {
           </div>
         </div>
       </section>
-
-      <section className="section">
+      {/* TODO */}
+      {/* <section className="section">
         <div className="section__content">
           <h2 className="section__header">Work.</h2>
           <div className="box section__description">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
         </div>
-      </section>
+      </section> */}
     </Layout>
   )
 }
