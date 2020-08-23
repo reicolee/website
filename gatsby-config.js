5114ecf2486f04951abfb040137c8cb6efe3c01d
Reico Lee <reico@withdouble.com>
2020-08-23 14:22:44 -0400

    🔧 fix config for svg with absolute path

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,7 @@ module.exports = {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
-          include: `src/svg`
+          include: `${__dirname}/src/svg`
         }
       }
     }
