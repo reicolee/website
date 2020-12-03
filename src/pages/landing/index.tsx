07b93ebfeb663e75b893c7e7cbd773cd4eec1442
Reico Lee <reico@withdouble.com>
2020-12-03 18:28:03 -0500

    ✨ improve about copies

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,16 +1,16 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+import { useStaticQuery, graphql } from "gatsby"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons"
 
-import portrait from 'src/images/portrait.jpg'
-import DoubleLogo from 'src/svg/double_logo.svg'
-import BentoBoxLogo from 'src/svg/bentobox-logo.svg'
+import portrait from "src/images/portrait.jpg"
+import DoubleLogo from "src/svg/double_logo.svg"
+import BentoBoxLogo from "src/svg/bentobox-logo.svg"
 
 import Layout from "src/components/Layout"
 import SEO from "src/components/SEO"
 
-import './style.scss'
+import "./style.scss"
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -22,7 +22,7 @@ const IndexPage = () => {
         }
       }
     }
-  `);
+  `)
 
   const { tagline, description } = data.site.siteMetadata
 
@@ -33,18 +33,48 @@ const IndexPage = () => {
         <div className="col col__1">
           <div className="tagline-section">
             <p className="greeting">Hi,</p>
-            <h1 className="heading">I'm <span>Reico</span> Lee.</h1>
+            <h1 className="heading">
+              I'm <span>Reico</span> Lee.
+            </h1>
             <h2 className="tagline">{tagline}</h2>
-            <p className="companies">Software Engineer <a className="companyLink" target="_blank" rel="nofollow noopener noreferrer" href="https://withdouble.com">@Double</a>.
-            Formerly <a className="companyLink" target="_blank" rel="nofollow noopener noreferrer" href="https://getbento.com/">@BentoBox</a>.</p>
-            <a className="resumeBtn" aria-label="Reico's Resume" target="_blank" rel="nofollow noopener noreferrer" href={'../../reico-resume.pdf'}>
+            <p className="companies">
+              Software Engineer{" "}
+              <a
+                className="companyLink"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+                href="https://withdouble.com"
+              >
+                @Double
+              </a>
+              . Formerly{" "}
+              <a
+                className="companyLink"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+                href="https://getbento.com/"
+              >
+                @BentoBox
+              </a>
+              .
+            </p>
+            <a
+              className="resumeBtn"
+              aria-label="Reico's Resume"
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+              href={"../../reico-resume.pdf"}
+            >
               View resumé
-              <FontAwesomeIcon icon={faExternalLinkAlt} className="external-link-icon"/>
+              <FontAwesomeIcon
+                icon={faExternalLinkAlt}
+                className="external-link-icon"
+              />
             </a>
           </div>
         </div>
         <div className="col col__2">
-          <img src={portrait} className="portrait" alt="Reico's portrait"/>
+          <img src={portrait} className="portrait" alt="Reico's portrait" />
         </div>
       </section>
       <section id="about" className="about__section">
@@ -52,13 +82,15 @@ const IndexPage = () => {
           <h2 className="about__section__header section-header">About.</h2>
           <div className="box about__section__description">
             <p>
-              I’m a full stack software engineer based in NYC. I love building software and solving hard problems. 
-              Regardless of how major and minor the task is, you will find me meticulously looking for a solution that fits the context.
+              I’m a product-minded software engineer based in NYC. I love
+              leveraging the use of technologies to leapfrog human/user
+              problems. Regardless of how major or minor a problem is, you will
+              find me meticulously looking for a solution that fits the context.
             </p>
             <p>
-              My core values and approaches are humility, resourcefulness, and performance-driven.
-              My ultimate goal is to translate my passion into software development, perpetually work on improving my chops, and build
-              great products that empower lives.
+              My core values are humility, proactivity, and results-driven. My
+              ultimate goal is to perpetually work on improving my chops, and
+              build great products that empower lives.
             </p>
           </div>
           <div className="box about__section__col1">
@@ -67,23 +99,40 @@ const IndexPage = () => {
               <li>JavaScript (ES6+)</li>
               <li>HTML & CSS (w/ Sass)</li>
               <li>Node.js</li>
-              <li>React + React Native</li>
+              <li>React, React Native</li>
               <li>Redux</li>
               <li>Web accessibility</li>
               <li>GraphQL</li>
               <li>Python 3</li>
               <li>Django (w/ DRF)</li>
-              <li>Netlify</li>
-              <li>AWS</li>
             </ul>
           </div>
           <div className="box about__section__col2">
             <h3 className="box-header">Facts.</h3>
             <ul>
-              <li>Born in <a target="_blank" rel="nofollow noopener noreferrer" href="https://en.wikipedia.org/wiki/Name_of_Toronto#Nicknames">the 6ix</a> 🇨🇦</li>
+              <li>
+                Born in{" "}
+                <a
+                  target="_blank"
+                  rel="nofollow noopener noreferrer"
+                  href="https://en.wikipedia.org/wiki/Name_of_Toronto#Nicknames"
+                >
+                  the 6ix
+                </a>{" "}
+                🇨🇦
+              </li>
               <li>Raised in +852 🇭🇰</li>
               <li>Trilingual</li>
-              <li>Graduated from <a target="_blank" rel="nofollow noopener noreferrer" href="https://www.sauder.ubc.ca/">UBC Sauder</a></li>
+              <li>
+                Graduated from{" "}
+                <a
+                  target="_blank"
+                  rel="nofollow noopener noreferrer"
+                  href="https://www.sauder.ubc.ca/"
+                >
+                  UBC Sauder
+                </a>
+              </li>
               <li>Walking my cats daily 🐈</li>
               <li>FIFA Ultimate Team 🎮</li>
             </ul>
@@ -98,9 +147,10 @@ const IndexPage = () => {
             <span className="work__section__item__tenure">2019 - Present</span>
             <DoubleLogo />
             <p className="box-text-center">
-              I collaborate closely with product, design, and customer success to iterate on 
-             core features development, work primarily in two React frontend applications that facilitate and improve the delegation experienece
-             between executives and assistants.
+              I collaborate closely with product, design, and customer success
+              to iterate on core features development, work primarily in two
+              React frontend applications that facilitate and improve the
+              delegation experienece between executives and assistants.
             </p>
           </div>
           <div className="box work__section__item work__section__item2">
@@ -108,15 +158,21 @@ const IndexPage = () => {
             <span className="work__section__item__tenure">2017 - 2019</span>
             <BentoBoxLogo />
             <p className="box-text-center">
-              I worked on a wide array of projects ranging from frontend, backend, or full stack. 
-              Noteable projects include improved data pipeline & analytics, implemented core accessibility features on 5000+ websites, 
-              streamlined company-wide productivity with better tooling, core CMS features development, and more.
+              I worked on a wide array of projects across the stack. Noteable
+              projects include improved data pipeline & analytics, implemented
+              core accessibility features on 5000+ websites, streamlined
+              company-wide productivity with better tooling, core CMS features
+              development, reporting tools for customers, and more.
             </p>
           </div>
           <div className="box work__section__item3">
             <h3 className="box-header">What's next?</h3>
-            <p className="box-text-center">Have questions? Want to learn more?</p>
-            <a href='mailto:leereico@gmail.com?body=Hi Reico!'>Let's connect!</a>
+            <p className="box-text-center">
+              Have questions? Want to learn more?
+            </p>
+            <a href="mailto:leereico@gmail.com?body=Hi Reico!">
+              Let's connect!
+            </a>
           </div>
         </div>
       </section>
@@ -124,11 +180,4 @@ const IndexPage = () => {
   )
 }
 
-
 export default IndexPage
-
-// import { Link } from "gatsby"
-// import Image from "../components/image"
-{/* 
-<Link to="/page-2/">Go to page 2</Link> <br />
-<Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
