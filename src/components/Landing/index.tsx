e05c48c994b6a0e09f3732b25a829a3d49d30bd6
Reico Lee <reico@withdouble.com>
2021-02-06 12:30:51 -0500

    ♻️ Landing Component

---
+++ src/components/Landing/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import "./style.scss"
+
+const Landing = ({ data }) => {
+  return <div></div>
+}
+
+Landing.propTypes = {
+  data: PropTypes.object,
+}
+
+export default Landing
