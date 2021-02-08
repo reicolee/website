f6597100c41d047e97fa2e9f7046ff00472258eb
Reico Lee <reico@withdouble.com>
2021-02-08 00:46:18 -0500

    ♻️ intro to css module

--- src/components/Landing/Intro/index.tsx
+++ src/components/Landing/Intro/index.tsx
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { useContext } from "react"
 import PropTypes from "prop-types"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons"
+import cn from "classnames"
 
+import Context from "src/store/context"
 import portrait from "src/images/portrait.jpg"
 
 const PROFESSION = "Software Engineer"
@@ -10,58 +12,61 @@ const FORMER_COMPANY = "@BentoBox"
 const CURRENT_COMPANY = "@Double"
 const CTA = "View resumé"
 
-import "./style.scss"
+import style from "./style.module.scss"
 
-const Intro = ({ tagline }) => (
-  <section className="intro">
-    <div className="col col__1">
-      <div className="tagline-section">
-        <p className="greeting">Hi,</p>
-        <h1 className="heading">
-          I'm <span>Reico</span> Lee.
-        </h1>
-        <h2 className="tagline">{tagline}</h2>
-        <p className="companies">
-          {PROFESSION}{" "}
+const Intro = ({ tagline }) => {
+  const { state } = useContext(Context)
+  return (
+    <section className={cn(style.intro, state.isDark && style.dark)}>
+      <div className={cn(style.col, style.left)}>
+        <div className={style.taglineSection}>
+          <p className={style.greeting}>Hi,</p>
+          <h1 className={style.heading}>
+            I'm <span>Reico</span> Lee.
+          </h1>
+          <h2 className={style.tagline}>{tagline}</h2>
+          <p className={style.companies}>
+            {PROFESSION}{" "}
+            <a
+              className={style.companyLink}
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+              href="https://withdouble.com"
+            >
+              {CURRENT_COMPANY}
+            </a>
+            . Formerly{" "}
+            <a
+              className={style.companyLink}
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+              href="https://getbento.com/"
+            >
+              {FORMER_COMPANY}
+            </a>
+            .
+          </p>
           <a
-            className="companyLink"
+            className={style.resumeBtn}
+            aria-label="Reico's Resume"
             target="_blank"
             rel="nofollow noopener noreferrer"
-            href="https://withdouble.com"
+            href={"../../reico-resume.pdf"}
           >
-            {CURRENT_COMPANY}
+            {CTA}
+            <FontAwesomeIcon
+              icon={faExternalLinkAlt}
+              className={style.externalLinkIcon}
+            />
           </a>
-          . Formerly{" "}
-          <a
-            className="companyLink"
-            target="_blank"
-            rel="nofollow noopener noreferrer"
-            href="https://getbento.com/"
-          >
-            {FORMER_COMPANY}
-          </a>
-          .
-        </p>
-        <a
-          className="resumeBtn"
-          aria-label="Reico's Resume"
-          target="_blank"
-          rel="nofollow noopener noreferrer"
-          href={"../../reico-resume.pdf"}
-        >
-          {CTA}
-          <FontAwesomeIcon
-            icon={faExternalLinkAlt}
-            className="external-link-icon"
-          />
-        </a>
+        </div>
       </div>
-    </div>
-    <div className="col col__2">
-      <img src={portrait} className="portrait" alt="Reico's portrait" />
-    </div>
-  </section>
-)
+      <div className={cn(style.col, style.right)}>
+        <img src={portrait} className={style.portrait} alt="Reico's portrait" />
+      </div>
+    </section>
+  )
+}
 
 Intro.propTypes = {
   tagline: PropTypes.string.isRequired,
