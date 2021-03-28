68d837697a4bd176624753ef8201d89362e7f767
Reico Lee <leereico@gmail.com>
2021-03-28 18:10:59 -0400

    :chart_with_upward_trend: tracking GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -116,14 +116,8 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "267027465", // Google Analytics / GA
+          "G-G8DVT2S9B8", // Google Analytics / GA
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
